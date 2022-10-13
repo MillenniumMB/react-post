1d9754234c9482a45d181fe2bd01b63f666f7661
Milena <Butovcenko01@bk.ru>
2022-10-14 00:11:52 +0300

    change page about

--- src/Page/About/About.jsx
+++ src/Page/About/About.jsx
@@ -4,10 +4,10 @@ const About = () => {
     return (
         <div className={css.Img}>
             <section className={css.section}>
-                <h6 className={css.title_01__subTitle}>Всем привет!</h6>
-                <h2 className={css.title_01__title}>Это мой проект для изучения React</h2>
+                <h6 className={css.title_}>Всем привет!</h6>
+                <h2 className={css.title_sub}>Это мой проект для изучения React</h2>
                 <div className={css.mainText}>
-                    Лучший способом понять
+                    Лучший способ понять язык, это пилить проекты!
                 </div>
             </section>
         </div>
